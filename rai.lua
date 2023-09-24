a4a1e6a4b518e155c4e1b6874f998e1062dc3fa1
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2023-09-24 12:02:44 -0300

    create rai.lua

---
+++ rai.lua
@@ -0,0 +1,36 @@
+local TweenService = game:GetService("TweenService")
+local TeleportService = game:GetService("TeleportService")
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+local chestCFrames = {
+    CFrame.new(-5369.15869, 312.698517, -3034.57349, 0.927179396, 0, 0.374617696, 0, 1, 0, -0.374617696, 0, 0.927179396),
+    CFrame.new(-5075.64355, 370.065948, -3174.44238, 0.374604106, -0, -0.92718488, 0, 1, -0, 0.92718488, 0, 0.374604106),
+    CFrame.new(-4919.99268, 370.065979, -2845.20337, 0.927179396, 0, 0.374617696, 0, 1, 0, -0.374617696, 0, 0.927179396),
+    CFrame.new(-5099.02441, 335.68045, -3169.30859, 0.92051065, 0, 0.390717506, 0, 1, 0, -0.390717506, 0, 0.92051065),
+    CFrame.new(5937.29248, 661.04657, 261.559326, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+}
+
+local function tweenToLocations()
+    local tweenInfo = TweenInfo.new(2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+
+    for _, cframe in ipairs(chestCFrames) do
+        local goal = {}
+        goal.CFrame = cframe
+
+        print("Movendo para a próxima localização...")
+        local tween = TweenService:Create(humanoidRootPart, tweenInfo, goal)
+        tween:Play()
+        tween.Completed:Wait()
+
+        wait(1)
+    end
+
+    wait(1)
+    local targetPlaceId = 2753915549
+    TeleportService:Teleport(targetPlaceId)
+end
+
+tweenToLocations()
