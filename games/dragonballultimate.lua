417b44c6b3a556ec2301f7d9961fe276c2a953dd
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 19:40:27 -0300

    update games/dragonballultimate.lua

--- games/dragonballultimate.lua
+++ games/dragonballultimate.lua
@@ -70,8 +70,44 @@ local espLib = {
     },
 };
 
+_G.espcheck = false
 _G.autotrain = false
 
+function espcheck()
+  if _G.espcheck == true then
+    espLib:Load()
+  else
+    espLib:Unload()
+  end
+end
+function autotrain1()
+    while _G.autotrain do
+        wait(0.0001)
+        local args = {"Blacknwhite27"}
+        game:GetService("ReplicatedStorage").Package.Events.def:InvokeServer(unpack(args))
+    end
+end
+function autotrain2()
+    while _G.autotrain do
+        wait(0.0001)
+        args = {1, false, CFrame.new(-477.7599182128906, 219.12937927246094, -3772.001953125) * CFrame.Angles(-3.141592502593994, -0.8255488276481628, -3.141592502593994)}
+        game:GetService("ReplicatedStorage").Package.Events.kb:FireServer(unpack(args))
+    end
+end
+function autotrain3()
+    while _G.autotrain do
+        wait(0.0001)
+        args = {"Blacknwhite27"}
+        game:GetService("ReplicatedStorage").Package.Events.cha:InvokeServer(unpack(args))
+    end
+end
+function autotrain4()
+    while _G.autotrain do
+        wait(0.0001)
+        args = {"Blacknwhite27", 1}
+        game:GetService("ReplicatedStorage").Package.Events.p:FireServer(unpack(args))
+    end
+end
 function checkprotocol()
   if _G.protocolprocesskeyrailibcheckscriptkey == false then
     RaiLib:Destroy()
@@ -120,35 +156,6 @@ MainFarmTab:AddButton({
   	end
 })
 
-function autotrain1()
-    while _G.autotrain do
-        wait(0.0001)
-        local args = {"Blacknwhite27"}
-        game:GetService("ReplicatedStorage").Package.Events.def:InvokeServer(unpack(args))
-    end
-end
-function autotrain2()
-    while _G.autotrain do
-        wait(0.0001)
-        args = {1, false, CFrame.new(-477.7599182128906, 219.12937927246094, -3772.001953125) * CFrame.Angles(-3.141592502593994, -0.8255488276481628, -3.141592502593994)}
-        game:GetService("ReplicatedStorage").Package.Events.kb:FireServer(unpack(args))
-    end
-end
-function autotrain3()
-    while _G.autotrain do
-        wait(0.0001)
-        args = {"Blacknwhite27"}
-        game:GetService("ReplicatedStorage").Package.Events.cha:InvokeServer(unpack(args))
-    end
-end
-function autotrain4()
-    while _G.autotrain do
-        wait(0.0001)
-        args = {"Blacknwhite27", 1}
-        game:GetService("ReplicatedStorage").Package.Events.p:FireServer(unpack(args))
-    end
-end
-
 MainEspTab:AddButton({
 	Name = "Esp Player",
 	Callback = function()
@@ -164,11 +171,4 @@ MainEspTab:AddButton({
   	end
 })
 
-function espcheck()
-  if _G.espcheck == true then
-    espLib:Load()
-  else
-    espLib:Unload()
-  end
-end
 RaiLib:Init()
