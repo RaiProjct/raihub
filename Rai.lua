b0e71378b202d599161a5239ab2e486d7a93b0dc
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 13:25:48 -0300

    update Rai.lua

--- Rai.lua
+++ Rai.lua
@@ -1,108 +1,110 @@
 local RaiLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/RaiProjct/raihub/main/RaiLib')))()
-local MainWindow = RaiLib:MakeWindow({
-  Name = "Privileged  |  Key System",
-  HidePremium = false,
-  IntroText = "Privileged | Carregando...",
-  SaveConfig = true,
-  ConfigFolder = "key-pg"
-})
+local MainWindow = RaiLib:MakeWindow{
+    Name = "Privileged  |  Key System",
+    HidePremium = false,
+    IntroText = "Privileged | Carregando...",
+    SaveConfig = true,
+    ConfigFolder = "key-pg"
+}
 
 local KeyData = {}
 local KeyType = "string"
-
 KeyData["Trial"] = true
 
 -- [ Funções de Verificação ] --
-function CheckGame()
-local gameId = game.PlaceID
-if gameId == 71315343 then
-dragonballrage()
-else
-  return
-end
-end
-
-function CheckKey()
-RaiLib:Destroy()
-RaiLib:MakeNotification({
-  Name = "Sucesso!",
-  Content = "Você injetou o script.",
-  Image = "rbxassetid://13132648697",
-  Time = 10
-})
-wait(3)
-CheckGame()
+local function CheckGame()
+    local gameId = game.PlaceID
+    if gameId == 71315343 then
+        dragonballrage()
+    else
+        return
+    end
 end
 
-function DisplayInvalidKeyNotification()
-RaiLib:MakeNotification({
-  Name = "Key Inválida",
-  Content = "Sua Key está inválida.",
-  Image = "rbxassetid://13132648697",
-  Time = 10
-})
+local function CheckKey()
+    RaiLib:Destroy()
+    RaiLib:MakeNotification{
+        Name = "Sucesso!",
+        Content = "Você injetou o script.",
+        Image = "rbxassetid://13132648697",
+        Time = 10
+    }
+    wait(3)
+    CheckGame()
+end
+
+local function DisplayInvalidKeyNotification()
+    RaiLib:MakeNotification{
+        Name = "Key Inválida",
+        Content = "Sua Key está inválida.",
+        Image = "rbxassetid://13132648697",
+        Time = 10
+    }
 end
 
 -- [ Configuração da Janela de Key System ] --
-local KeySystemTab = MainWindow:MakeTab({
-  Name = "Key System",
-  Icon = "rbxassetid://4483345998",
-  PremiumOnly = false
-})
+local KeySystemTab = MainWindow:MakeTab{
+    Name = "Key System",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+}
 
-KeySystemTab:AddTextbox({
-  Name = "Enter Key",
-  Default = "",
-  TextDisappear = true,
-  Callback = function(Value)
-  KeyType = Value
-  end
-})
+KeySystemTab:AddTextbox{
+    Name = "Enter Key",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+        KeyType = Value
+    end
+}
 
-KeySystemTab:AddButton({
-  Name = "Login",
-  Callback = function()
-  if KeyData[KeyType] then
-  CheckKey()
-  else
-    DisplayInvalidKeyNotification()
-  end
-  end
-})
+KeySystemTab:AddButton{
+    Name = "Login",
+    Callback = function()
+        if KeyData[KeyType] then
+            CheckKey()
+        else
+            DisplayInvalidKeyNotification()
+        end
+    end
+}
 
 -- [ Função dos games ] --
-function dragonballrage()
-local dbrWindow = RaiLib:MakeWindow({
-  Name = "Privileged  | Dragon Ball Rage",
-  HidePremium = false,
-  IntroText = "Privileged | Carregando...",
-  SaveConfig = true,
-  ConfigFolder = "dbr-pg"
-})
+local function dragonballrage()
+    local dbrWindow = RaiLib:MakeWindow{
+        Name = "Privileged  | Dragon Ball Rage",
+        HidePremium = false,
+        IntroText = "Privileged | Carregando...",
+        SaveConfig = true,
+        ConfigFolder = "dbr-pg"
+    }
 
-local dbrFarmTab = dbrWindow:MakeTab({
-  Name = "Farm",
-  Icon = "rbxassetid://4483345998",
-  PremiumOnly = false
-})
-local dbrEspTab = dbrWindow:MakeTab({
-  Name = "Esp",
-  Icon = "rbxassetid://4483345998",
-  PremiumOnly = false
-})
+    local dbrFarmTab = dbrWindow:MakeTab{
+        Name = "Farm",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    }
 
-dbrFarmTab:AddToggle({
-  Name = "Auto Train",
-  Default = false,
-  Callback = function(Value)
-  _G.autotrain = Value
-  autotrain()
-  end
-})
-function autostats()
-while _G.autotrain == true do
-end
-end
+    local dbrEspTab = dbrWindow:MakeTab{
+        Name = "Esp",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    }
+
+    dbrFarmTab:AddToggle{
+        Name = "Auto Train",
+        Default = false,
+        Callback = function(Value)
+            _G.autotrain = Value
+            autotrain()
+        end
+    }
+
+    local function autotrain()
+        while _G.autotrain == true do
+            -- Autotrain logic
+        end
+    end
 end
 
 RaiLib:Init()
