79791c06ce7e9131b9f93a3f1609b137be7fac24
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 13:07:56 -0300

    update Rai.lua

--- Rai.lua
+++ Rai.lua
@@ -1,10 +1,10 @@
 local RaiLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/RaiProjct/raihub/main/RaiLib')))()
 local MainWindow = RaiLib:MakeWindow({
-    Name = "Privileged  |  Key System",
-    HidePremium = false,
-    IntroText = "Privileged | Carregando...",
-    SaveConfig = true,
-    ConfigFolder = "key-pg"
+  Name = "Privileged  |  Key System",
+  HidePremium = false,
+  IntroText = "Privileged | Carregando...",
+  SaveConfig = true,
+  ConfigFolder = "key-pg"
 })
 
 local KeyData = {}
@@ -14,71 +14,71 @@ KeyData["Trial"] = true
 
 -- [ Funções de Verificação ] --
 function CheckGame()
-    local gameId = game.PlaceID
-    if gameId == 6442957604 then
-        pctycoon()
-    else
-        return
-    end
+local gameId = game.PlaceID
+if gameId == 6442957604 then
+pctycoon()
+else
+  return
+end
 end
 
 function CheckKey()
-    RaiLib:Destroy()
-    RaiLib:MakeNotification({
-        Name = "Sucesso!",
-        Content = "Você injetou o script.",
-        Image = "rbxassetid://13132648697",
-        Time = 10
-    })
-    wait(3)
-    CheckGame()
+RaiLib:Destroy()
+RaiLib:MakeNotification({
+  Name = "Sucesso!",
+  Content = "Você injetou o script.",
+  Image = "rbxassetid://13132648697",
+  Time = 10
+})
+wait(3)
+CheckGame()
 end
 
 function DisplayInvalidKeyNotification()
-    RaiLib:MakeNotification({
-        Name = "Key Inválida",
-        Content = "Sua Key está inválida.",
-        Image = "rbxassetid://13132648697",
-        Time = 10
-    })
+RaiLib:MakeNotification({
+  Name = "Key Inválida",
+  Content = "Sua Key está inválida.",
+  Image = "rbxassetid://13132648697",
+  Time = 10
+})
 end
 
 -- [ Configuração da Janela de Key System ] --
 local KeySystemTab = MainWindow:MakeTab({
-    Name = "Key System",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
+  Name = "Key System",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 KeySystemTab:AddTextbox({
-    Name = "Enter Key",
-    Default = "",
-    TextDisappear = true,
-    Callback = function(Value)
-        KeyType = Value
-    end
+  Name = "Enter Key",
+  Default = "",
+  TextDisappear = true,
+  Callback = function(Value)
+  KeyType = Value
+  end
 })
 
 KeySystemTab:AddButton({
-    Name = "Login",
-    Callback = function()
-        if KeyData[KeyType] then
-            CheckKey()
-        else
-            DisplayInvalidKeyNotification()
-        end
-    end
+  Name = "Login",
+  Callback = function()
+  if KeyData[KeyType] then
+  CheckKey()
+  else
+    DisplayInvalidKeyNotification()
+  end
+  end
 })
 
 -- [ Função do pctycoon ] --
-function pctycoon()
-    local pctycoonWindow = RaiLib:MakeWindow({
-        Name = "Privileged  | pctycoon",
-        HidePremium = false,
-        IntroText = "Privileged | Carregando...",
-        SaveConfig = true,
-        ConfigFolder = "pctycoon-pg"
-    })
+function dragonballrage()
+local dbrWindow = RaiLib:MakeWindow({
+  Name = "Privileged  | pctycoon",
+  HidePremium = false,
+  IntroText = "Privileged | Carregando...",
+  SaveConfig = true,
+  ConfigFolder = "pctycoon-pg"
+})
 end
 
 RaiLib:Init()
