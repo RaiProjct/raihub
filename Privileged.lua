5934c50165773c55bf07d939cad064b0c29c0b36
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 18:17:32 -0300

    update Privileged.lua

--- Privileged.lua
+++ Privileged.lua
@@ -13,7 +13,7 @@ _G.KeyType = "string"
 
 -- [ Funções de Verificação ] --
 function process()
-    _G.gameidprishsjsjsks = game.PlaceID
+    _G.gameidprishsjsjsks = game.PlaceId
     print(gameidprishsjsjsks)
     RaiLib:MakeNotification{
         Name = "Sucesso!",
