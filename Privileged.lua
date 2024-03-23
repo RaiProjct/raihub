535c130e2cc88b1a0c50d1e900e92e11383dd592
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 18:55:24 -0300

    update Privileged.lua

--- Privileged.lua
+++ Privileged.lua
@@ -60,31 +60,4 @@ KeySystemTab:AddButton{
     end
 }
 
-function nextpage()
-
-local Main2Window = RaiLib:MakeWindow{
-    Name = "Privileged  |  Select Game",
-    HidePremium = false,
-    IntroText = "Privileged | Carregando...",
-    SaveConfig = true,
-    ConfigFolder = "sg-pg"
-}
-
-local GameTab = Main2Window:MakeTab{
-    Name = "Games",
-    Icon = "rbxassetid://16845953528",
-    PremiumOnly = false
-}
-
-GameTab:AddButton{
-    Name = "Dragon Blox Ultimate",
-    Callback = function()
-      RaiLib:Destroy()
-      loadstring(game:HttpGet(("https://raw.githubusercontent.com/RaiProjct/raihub/main/games/dragonballultimate.lua")))()
-    end
-}
-
-RaiLib:Init()
-end
-
 RaiLib:Init()
