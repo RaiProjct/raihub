1265511f7e23e782c5b5bbec0eeb534ad8bce709
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 21:11:14 -0300

    update games/aimbotuniversal.lua

--- games/aimbotuniversal.lua
+++ games/aimbotuniversal.lua
@@ -1,2 +1,48 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()()
 ExunysDeveloperAimbot.Load()
+
+getgenv().ExunysDeveloperAimbot = {
+	DeveloperSettings = {
+		UpdateMode = "RenderStepped",
+		TeamCheckOption = "TeamColor",
+		RainbowSpeed = 1 -- Bigger = Slower
+	},
+
+	Settings = {
+		Enabled = true,
+
+		TeamCheck = false,
+		AliveCheck = true,
+		WallCheck = false,
+
+		OffsetToMoveDirection = false,
+		OffsetIncrement = 15, -- Min: 1; Max: 30
+
+		Sensitivity = 1.5, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity2 = 3, -- mousemoverel Sensitivity
+
+		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
+		LockPart = "Head", -- Body part to lock on
+
+		TriggerKey = Enum.UserInputType.MouseButton2,
+		Toggle = false
+	},
+
+	FOVSettings = {
+		Enabled = true,
+		Visible = true,
+
+		Radius = 160, -- Field Of View
+		NumSides = 60,
+
+		Thickness = 1,
+		Transparency = 1,
+		Filled = false,
+
+		RainbowColor = false,
+		RainbowOutlineColor = false,
+		Color = Color3.fromRGB(255, 255, 255),
+		OutlineColor = Color3.fromRGB(0, 0, 0),
+		LockedColor = Color3.fromRGB(255, 150, 150)
+	}
+}
