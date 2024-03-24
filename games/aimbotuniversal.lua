aeb52ade9cb4974aa1c82927b3521a8106e30108
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 21:47:45 -0300

    update games/aimbotuniversal.lua

---
+++ games/aimbotuniversal.lua
@@ -0,0 +1,9 @@
+local RaiLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/RaiProjct/raihub/main/RaiLib")))()
+
+local MainWindow = RaiLib:MakeWindow{
+    Name = "Privileged  |  Aimbot Universal",
+    HidePremium = false,
+    IntroText = "Privileged | Carregando...",
+    SaveConfig = true,
+    ConfigFolder = "aimbot-pg"
+}
