3f2bf5c041c33f92d2f292de6d5f2919c48fe98a
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 21:50:24 -0300

    update games/aimbotuniversal.lua

--- games/aimbotuniversal.lua
+++ games/aimbotuniversal.lua
@@ -1,5 +1,7 @@
 local RaiLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/RaiProjct/raihub/main/RaiLib")))()
 
+_G.systemEnabled = false
+
 local MainWindow = RaiLib:MakeWindow{
     Name = "Privileged  |  Aimbot Universal",
     HidePremium = false,
@@ -17,6 +19,9 @@ local Config = MainWindow:MakeTab{
 Config:AddButton{
     Name = "Aimbot",
     Callback = function()
-      
+      if _G.systemEnabled == false then
+        _G.systemEnabled = true
+      else
+        _G.systemEnabled = false
     end
 }
