3d04031a6d5ac718d1bd3f40913ab60754db41a1
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-23 21:10:05 -0300

    update games/aimbotuniversal.lua

--- games/aimbotuniversal.lua
+++ games/aimbotuniversal.lua
@@ -1,2 +1,2 @@
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
-Aimbot.Load()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()()
+ExunysDeveloperAimbot.Load()
