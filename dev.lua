7d16dbc78e21304b537ccf1238dcfc42535eadd3
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2024-03-26 18:14:29 -0300

    update dev.lua

--- dev.lua
+++ dev.lua
@@ -1,8 +1,8 @@
 -- Credit Dollynho --
 
-_G.aimbotenabled = false
+_G.aimtoggled = false
 
-local fov = 60
+local fov = 40
 local maxDistance = 400
 local maxTransparency = 0.1
 local teamCheck = false
@@ -55,40 +55,30 @@ local function isPlayerAlive(player)
 end
 
 local function getClosestPlayerInFOV(trg_part)
-    if _G.aimbotenabled then
-      FOVring.Visible = true
-      local nearest = nil
-      local last = math.huge
-      local playerMousePos = Cam.ViewportSize / 2
-      local localPlayer = Players.LocalPlayer
-  
-      for i = 1, #Players:GetPlayers() do
-          local player = Players:GetPlayers()[i]
-          if player and player ~= localPlayer and (not teamCheck or player.Team ~= localPlayer.Team) then
-              if isPlayerAlive(player) then
-                  local part = player.Character and player.Character:FindFirstChild(trg_part)
-                  if part then
-                      local ePos, isVisible = Cam:WorldToViewportPoint(part.Position)
-                      local distance = (Vector2.new(ePos.x, ePos.y) - playerMousePos).Magnitude
-  
-                      if distance < last and isVisible and distance < fov and distance < maxDistance then
-                          last = distance
-                          local nearest = player
-                      end
-                  end
-              end
-          end
-      end
-      print(_G.aimbotenabled)
-      print(nearest)
-      return nearest
-    else
-      nearest = nil
-      FOVring.Visible = false
-      print(_G.aimbotenabled)
-      print(nearest)
-      return nearest
+    local nearest = nil
+    local last = math.huge
+    local playerMousePos = Cam.ViewportSize / 2
+    local localPlayer = Players.LocalPlayer
+
+    for i = 1, #Players:GetPlayers() do
+        local player = Players:GetPlayers()[i]
+        if player and player ~= localPlayer and (not teamCheck or player.Team ~= localPlayer.Team) then
+            if isPlayerAlive(player) then
+                local part = player.Character and player.Character:FindFirstChild(trg_part)
+                if part then
+                    local ePos, isVisible = Cam:WorldToViewportPoint(part.Position)
+                    local distance = (Vector2.new(ePos.x, ePos.y) - playerMousePos).Magnitude
+
+                    if distance < last and isVisible and distance < fov and distance < maxDistance then
+                        last = distance
+                        nearest = player
+                    end
+                end
+            end
+        end
     end
+
+    return nearest
 end
 
 local function toggleTeamCheck()
@@ -102,7 +92,11 @@ RunService.RenderStepped:Connect(function()
     updateDrawings()
     local closest = getClosestPlayerInFOV("Head")
     if closest and closest.Character:FindFirstChild("Head") then
-        lookAt(closest.Character.Head.Position)
+        if _G.aimtoggled then
+          lookAt(closest.Character.Head.Position)
+        else
+          return nil
+        end
     end
     
     if closest then
